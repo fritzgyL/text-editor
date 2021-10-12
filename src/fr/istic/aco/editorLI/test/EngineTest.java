0ac2f6471f2e9cf19f7cad3dfaf2dbd8b422ee5a
Fritzgy Lubin <77797669+flubin99@users.noreply.github.com>
2021-10-12 16:39:54 +0200

    Initial commit

---
+++ src/fr/istic/aco/editorLI/test/EngineTest.java
@@ -0,0 +1,56 @@
+package fr.istic.aco.editorLI.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import fr.istic.aco.editorLI.model.Engine;
+import fr.istic.aco.editorLI.model.EngineImpl;
+import fr.istic.aco.editorLI.model.Selection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EngineTest {
+
+    private Engine engine;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        engine = new EngineImpl();
+    }
+
+    private void todo() {
+        fail("Unimplemented test");
+    }
+    @Test
+    @DisplayName("Buffer must be empty after initialisation")
+    void getSelection() {
+        Selection selection = engine.getSelection();
+        assertEquals(selection.getBufferBeginIndex(),selection.getBeginIndex());
+        assertEquals("",engine.getBufferContents());
+    }
+
+    @Test
+    void getBufferContents() {
+        todo();
+    }
+
+    @Test
+    void getClipboardContents() {
+        todo();
+    }
+
+    @Test
+    void cutSelectedText() {
+        todo();
+    }
+
+    @Test
+    void copySelectedText() {
+        todo();
+    }
+
+    @Test
+    void pasteClipboard() {
+        todo();
+    }
+}
